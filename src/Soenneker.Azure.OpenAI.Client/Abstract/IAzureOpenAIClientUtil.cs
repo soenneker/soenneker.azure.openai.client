a7f142ff8d0bc7dfb746ba199c648188b82845ee
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:35:56 -0500

    xml commenting

--- src/Soenneker.Azure.OpenAI.Client/Abstract/IAzureOpenAIClientUtil.cs
+++ src/Soenneker.Azure.OpenAI.Client/Abstract/IAzureOpenAIClientUtil.cs
@@ -16,5 +16,10 @@ public interface IAzureOpenAIClientUtil : IDisposable, IAsyncDisposable
     /// <param name="options"></param>
     void SetOptions(AzureOpenAIClientOptions options);
 
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<AzureOpenAIClient> Get(CancellationToken cancellationToken = default);
 }
